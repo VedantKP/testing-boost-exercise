797dc5f6776a04cde8e1d9c9f0acd1b045ff5385
Vedant Puranik <vedant.puranik@gmail.com>
2025-02-01 16:24:49 +0100

    Formatted MatrixIOTest using clang-format

--- tests/MatrixIOTest.cpp
+++ tests/MatrixIOTest.cpp
@@ -1,8 +1,9 @@
-#include <boost/test/unit_test.hpp>
 #include <Eigen/Dense>
-#include <fstream>
+#include <boost/test/unit_test.hpp>
 #include <cstdio>
+#include <fstream>
 #include "../src/matrixIO.hpp"
+
 
 #define BOOST_TEST_DYN_LINK
 
@@ -11,30 +12,32 @@ using namespace std;
 using namespace matrixIO;
 
 // Function to delete CSV file after testing
-void deleteTestCSV(const string &filename) {
-    remove(filename.c_str());
+void deleteTestCSV(const string &filename)
+{
+  remove(filename.c_str());
 }
 
 BOOST_AUTO_TEST_SUITE(MatrixIOTests, *boost::unit_test::tolerance(1e-5))
 
 // Function tests if read csv data is as expected
-BOOST_AUTO_TEST_CASE(TestOpenData) {
-    string testFileName = "tests/test_matrix.csv";
-    
-    MatrixXd expectedMatrix(3, 3);
-    expectedMatrix << 1.232, 2.112, 3.312,
-                  4.312, 5.367, 6.495,
-                  7.212, 8.749, 9.363;
+BOOST_AUTO_TEST_CASE(TestOpenData)
+{
+  string testFileName = "tests/test_matrix.csv";
 
-    saveData(testFileName, expectedMatrix);
+  MatrixXd expectedMatrix(3, 3);
+  expectedMatrix << 1.232, 2.112, 3.312,
+      4.312, 5.367, 6.495,
+      7.212, 8.749, 9.363;
 
-    MatrixXd result = openData(testFileName, 3);
+  saveData(testFileName, expectedMatrix);
 
-    // Check if read values are as expected
-    BOOST_CHECK(result.isApprox(expectedMatrix));
+  MatrixXd result = openData(testFileName, 3);
 
-    // Temp file deleted after checking
-    deleteTestCSV(testFileName);
+  // Check if read values are as expected
+  BOOST_CHECK(result.isApprox(expectedMatrix));
+
+  // Temp file deleted after checking
+  deleteTestCSV(testFileName);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
